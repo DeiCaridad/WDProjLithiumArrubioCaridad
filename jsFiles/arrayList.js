a4c3cf4ff82e5ae2b09362c480eb01aff073ba70
DeiCaridad <b2029dpqcaridad@pshs.edu.ph>
2025-11-07 22:00:57 +0800

    started with the javascript portion of the home page. i hate it

---
+++ jsFiles/arrayList.js
@@ -0,0 +1,35 @@
+/* this is gonna be so messy 
+
+arrays below will be sorted by page according to the README file in the github repo. (e.x. p1 = home page, p2 = team builder, etc)
+*/
+
+
+/*P1*/
+    export const Events = [ 
+  { name: "Pokémon GO Tour: Unova (New Taipei City)", link: "https://gotour.pokemongolive.com/?hl=en" },
+  { name: "Festival of Colors 2025 (Pokémon GO)", link: "https://www.polygon.com/pokemon-go-guide/538952/festival-of-colors-2025-timed-research-bruxish-smeargle" },
+  { name: "Pokémon GO Fest 2025: Global", link: "https://pokemongolive.com/gofest/global" },
+  { name: "9th Anniversary Party (Pokémon GO)", link: "https://pokemongohub.net/post/event/pokemon-go-july-2025-events/" },
+  { name: "Dark Skies Event (Pokémon GO)", link: "https://pokemongohub.net/post/event/pokemon-go-august-2025-events/" },
+  { name: "Pokémon GO Fest 2025: Max Finale", link: "https://pokemongohub.net/post/event/pokemon-go-august-2025-events/" },
+  { name: "Kanto Celebration Event (Pokémon GO)", link: "https://pokemongohub.net/post/event/pokemon-go-september-2025-events/" },
+  { name: "Flabébé Community Day (Pokémon GO)", link: "https://pokemongohub.net/post/event/pokemon-go-september-2025-events/" },
+  { name: "Raid Day (Sept 7, 2025)", link: "https://pokemongo.com/en/post/save-the-date-s20" },
+  { name: "Shadow Raid Weekend (Sept 20-21, 2025)", link: "https://pokemongo.com/en/post/save-the-date-s20" },
+  { name: "Raid Day (Sept 28, 2025)", link: "https://pokemongo.com/en/post/save-the-date-s20" },
+  { name: "Raid Day (Oct 4, 2025)", link: "https://pokemongo.com/en/post/save-the-date-s20" },
+  { name: "Raid Day (Oct 18, 2025)", link: "https://pokemongo.com/en/post/save-the-date-s20" },
+  { name: "Max Battle Day (Nov 1, 2025)", link: "https://pokemongo.com/en/post/save-the-date-s20" },
+  { name: "Max Battle Weekend (Nov 21–24, 2025)", link: "https://pokemongo.com/en/post/save-the-date-s20" },
+  { name: "Max Battle Weekend (Nov 29–30, 2025)", link: "https://pokemongo.com/en/post/save-the-date-s20" },
+  { name: "Pokémon World Championships 2025", link: "https://worlds.pokemon.com/en-us" },
+  { name: "Regional & Special Championships 2025 (Play! Pokémon)", link: "https://www.pokemon.com/us/play-pokemon/pokemon-events/championship-series/2025/regional-special-championships" },
+  { name: "Wild Area 2025 (Japan/Global Event)", link: "https://www.nintendoworldreport.com/news/72205/pokemon-go-announces-2025-wild-area-event" },
+  { name: "Reddit Community Calendar (Fan Compilation)", link: "https://www.reddit.com/r/TheSilphRoad/comments/1l4kj41/june_2025_event_calendarrr/" },
+  { name: "Pokémon Legends: Z-A – Mega Dimension DLC", link: "https://www.theverge.com/games/815416/pokemon-legends-za-mega-dimension-release-date" },
+  { name: "Pokémon TCG Pocket: Extradimensional Crisis Pack", link: "https://www.polygon.com/pokemon/600812/pokemon-tcg-pocket-ultra-beasts-pack" },
+  { name: "Pokémon Champions (Upcoming Game 2026)", link: "https://www.wired.com/story/pokemon-company-announces-pokemon-champions-promises-legends-z-a-coming-in-2025" }];
+
+    export const TitleTop = [
+        
+    ]
--- jsFiles/homePage.js
+++ jsFiles/homePage.js
@@ -1,5 +1,18 @@
+import {Events} from './arrayList';
+
 function tbuild(){
     window.open("../pages/teambuilder.html",'_blank');
+}
+
+function textChange(){
+    let textTop1 = document.getElementById("textTop1");
+    let textTop2 = document.getElementById("textTop2");
+    let textTop3 = document.getElementById("textTop3");
+    let textTop4 = document.getElementById("textTop4");
+
+    let eventName = Events.map(events => events.name);
+
+    return document.getElementById("textTop1").innerHTML = eventName[Math.floor(Math.random() * eventName.length)], document.getElementById("textTop2").innerHTML = eventName[Math.floor(Math.random() * eventName.length)], document.getElementById("textTop3").innerHTML = eventName[Math.floor(Math.random() * eventName.length)], document.getElementById("textTop4").innerHTML = eventName[Math.floor(Math.random() * eventName.length)];
 }
 /*source
 
