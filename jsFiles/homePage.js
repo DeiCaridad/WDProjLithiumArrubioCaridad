dd4a9530e3dbe4ca778742498e864b92d62cb967
DeiCaridad <b2029dpqcaridad@pshs.edu.ph>
2025-11-07 11:06:11 +0800

    started with highlights + added team builder page, finished js portion for title

---
+++ jsFiles/homePage.js
@@ -0,0 +1,9 @@
+function tbuild(){
+    window.open("../pages/teambuilder.html",'_blank');
+}
+/*source
+
+https://stackoverflow.com/questions/1574008/how-to-simulate-target-blank-in-javascript
+thank you stack overflow
+
+*/
