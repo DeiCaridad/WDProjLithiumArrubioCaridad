2f3b04dddd75effb77f2c0ffd1dbd5c8beee503d
DeiCaridad <b2029dpqcaridad@pshs.edu.ph>
2025-12-11 09:51:55 +0800

    nicco wants me to commit :sob:

---
+++ jsFiles/teamBuilder.js
@@ -0,0 +1,10 @@
+/*
+idea: have t/f variable and while false it has + as default image
+set to true after picked variable is set to true
+*/
+
+cardVisual = false;
+
+while (cardVisual == false){
+    
+}
